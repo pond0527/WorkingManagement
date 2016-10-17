a1d9c7ef4d56a782a3853c3e0ccd972a5a06c2a2
pond0527 <like4869oge@yahoo.co.jp>
2016-10-18 00:46:26 +0900

    Initial Commit

---
+++ WorkingManagement/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  WorkingManagement
+//
+//  Created by 池田哲 on 2016/10/18.
+//  Copyright © 2016年 池田哲. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
